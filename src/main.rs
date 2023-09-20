d20f998f717abf165a2ba5eafa23cdc527c4f357
Iru21 <33987046+Iru21@users.noreply.github.com>
2023-09-20 13:23:32 +0200

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+    println!("Server listening on port 4221");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut data) => {
+                println!("Connection established!");
+                data.read(&mut [0; 128]).unwrap();
+                data.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
